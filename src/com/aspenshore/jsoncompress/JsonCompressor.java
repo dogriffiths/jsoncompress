ef76b72057844b9fa74a447812496a0983f2c5f6
David Griffiths <dogriffiths@gmail.com>
2015-10-23 20:14:46 +0100

    Started to write real code

--- src/com/aspenshore/jsoncompress/JsonCompressor.java
+++ src/com/aspenshore/jsoncompress/JsonCompressor.java
@@ -1,2 +1,33 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class JsonCompressor {
+    public byte[] compress(byte[] sourceBytes) {
+        String input = "";
+        for (byte sourceByte : sourceBytes) {
+            input = toBinary(sourceByte).substring(1,8) + input;
+        }
+        List<Byte> results = new ArrayList<Byte>();
+        while(input.length() > 0) {
+            int len = input.length();
+            int start = len - 8;
+            if (start < 0) {
+                start = 0;
+            }
+            String sevenBits = input.substring(start, len);
+            input = input.substring(0, start);
+            Integer integer = Integer.valueOf(sevenBits, 2);
+            byte b = (byte)(integer & 0xff);
+            results.add(b);
+        }
+        byte[] finalResult = new byte[results.size()];
+        for (int i = 0; i < finalResult.length; i++) {
+            finalResult[i] = results.get(i);
+        }
+        return finalResult;
+    }
+
+    private String toBinary(byte sourceByte) {
+        return String.format("%8s", Integer.toBinaryString(sourceByte & 0xFF)).replace(' ', '0');
+    }    
 }
--- test/com/aspenshore/jsoncompress/JsonCompressorTest.java
+++ test/com/aspenshore/jsoncompress/JsonCompressorTest.java
@@ -2,8 +2,19 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class JsonCompressorTest {
+
     @Test
-    public void dummy() {
-        Assert.fail("Write a test!");
+    public void canCompressEightBytes() {
+        byte[] bytes = new byte[]{0x7f, 0x7f, 0x7f, 0x7f, 0x7f, 0x7f, 0x7f, 0x7f};
+        JsonCompressor jsonCompressor = new JsonCompressor();
+        byte[] compress = jsonCompressor.compress(bytes);
+        Assert.assertEquals(7, compress.length);
+        Assert.assertEquals(0xff, compress[0] & 0xff);
+        Assert.assertEquals(0xff, compress[1] & 0xff);
+        Assert.assertEquals(0xff, compress[2] & 0xff);
+        Assert.assertEquals(0xff, compress[3] & 0xff);
+        Assert.assertEquals(0xff, compress[4] & 0xff);
+        Assert.assertEquals(0xff, compress[5] & 0xff);
+        Assert.assertEquals(0xff, compress[6] & 0xff);
     }
 }
